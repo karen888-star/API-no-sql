4e8f0d730b50cee3c051665bc7ea5bf73fa619d8
karen888-star <karenborda888@gmail.com>
2025-05-27 03:58:34 +0000

    commit

--- proyecto/src/firebaseConfig.js
+++ proyecto/src/firebaseConfig.js
@@ -1,13 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC4gnXGBu4DYmeaxLeE0Xmu6IuDyciyK_s",
+ apiKey: "AIzaSyC4gnXGBu4DYmeaxLeE0Xmu6IuDyciyK_s",
   authDomain: "harrypoterapi.firebaseapp.com",
   projectId: "harrypoterapi",
   storageBucket: "harrypoterapi.firebasestorage.app",
@@ -16,6 +12,8 @@ const firebaseConfig = {
   measurementId: "G-JB7ZHJBY2F"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app); // ✅ ¡Esto es necesario!
+
+export { auth, db };
