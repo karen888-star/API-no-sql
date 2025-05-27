45d9b5e400349f2b6ef24195ea8b4f298c442ed4
karen888-star <karenborda888@gmail.com>
2025-05-27 03:27:40 +0000

    commit

--- proyecto/src/firebaseConfig.js
+++ proyecto/src/firebaseConfig.js
@@ -1,18 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "aqui su codigo",
-authDomain: "aqui su codigo",
-projectId: "aqui su codigo",
-storageBucket: "aqui su codigo",
-messagingSenderId: "aqui su codigo",
-appId: "aqui su codigo"
+  apiKey: "AIzaSyC4gnXGBu4DYmeaxLeE0Xmu6IuDyciyK_s",
+  authDomain: "harrypoterapi.firebaseapp.com",
+  projectId: "harrypoterapi",
+  storageBucket: "harrypoterapi.firebasestorage.app",
+  messagingSenderId: "1089078510369",
+  appId: "1:1089078510369:web:c72af065fed7505001f460",
+  measurementId: "G-JB7ZHJBY2F"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app); // ✅ ¡Esto es necesario!
-
-export { auth, db };
+const analytics = getAnalytics(app);
